02fbb84b34e1d135b551ecb97192f6f607def9ad
gerrit developer <dev@example.com>
2018-05-15 20:37:58 +0200

    changing docker hub user
    Change-Id: Idcb9899e22da8eef21ac47cd7fb494e94cbfb4a4

--- build-merge-image.sh
+++ build-merge-image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --build-arg GIT_COMMIT=$(git rev-parse HEAD) --build-arg COMMIT_DATE=$(git log -1 --format=%cd --date=format:%Y-%m-%dT%H:%M:%S) -f Dockerfile_Merge -t jlojosnegros/curso-ci-practica:dev .
+docker build --build-arg GIT_COMMIT=$(git rev-parse HEAD) --build-arg COMMIT_DATE=$(git log -1 --format=%cd --date=format:%Y-%m-%dT%H:%M:%S) -f Dockerfile_Merge -t practicacimasterupm/curso-ci-practica:dev .
