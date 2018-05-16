606af880394a3107756e28f97db727fb60a14287
gerrit developer <dev@example.com>
2018-05-16 15:49:34 +0200

    new script
    Change-Id: Ibfadd1cd153f1b32b3d0468eb10077d6f25cb5f1

---
+++ bnp_ni.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+tag=$1
+docker build --build-arg GIT_COMMIT=$(git rev-parse HEAD) --build-arg COMMIT_DATE=$(git log -1 --format=%cd --date=format:%Y-%m-%dT%H:%M:%S) -f Dockerfile_Nightly -t practicacimasterupm/curso-ci-practica:${tag} .
+
+ docker push practicacimasterupm/curso-ci-practica:${tag}
