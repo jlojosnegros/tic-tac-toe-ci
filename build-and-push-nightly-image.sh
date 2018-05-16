33a129bc6267a75563d95fe7283f2c105673640d
gerrit developer <dev@example.com>
2018-05-16 11:17:11 +0200

    Adding elements for Nightly build
    Change-Id: Ia6d70800d76b35e2823dff354e7093e0fb271b81

---
+++ build-and-push-nightly-image.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+current_pom_version=`mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec`
+date=`date +%Y%m%d`
+string='nightly'
+
+
+match_expression='^([0-9]*)\.([0-9]*)\.([0-9]*)-(.*)$'
+
+if [[ ${current_pom_version} =~ $match_expression ]]; then
+    tag=${BASH_REMATCH[1]}\.${BASH_REMATCH[2]}\.${BASH_REMATCH[3]}\.${string}\.${date}
+fi
+
+echo "tag=$tag"
+
+docker build --build-arg GIT_COMMIT=$(git rev-parse HEAD) --build-arg COMMIT_DATE=$(git log -1 --format=%cd --date=format:%Y-%m-%dT%H:%M:%S) -f Dockerfile_Nightly -t practicacimasterupm/curso-ci-practica:${tag} .
+
+docker push practicacimasterupm/curso-ci-practica:${tag}
