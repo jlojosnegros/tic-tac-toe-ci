944b613fc2198511ef8a98de0698b46aaf363375
gerrit developer <dev@example.com>
2018-05-16 14:58:10 +0200

    testing
    Change-Id: I6b8419f3ee7eb7410b4cc4104873f0a51b805722

--- build-and-push-nightly-image.sh
+++ build-and-push-nightly-image.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 #current_pom_version=`mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec`
-#echo "input="$1
+echo "input="$1
 current_pom_version=$1
+echo "current_pom_version="$current_pom_version
 date=`date +%Y%m%d`
+echo "date="$date
 string='nightly'
 
 #echo "current_pom_version=$current_pom_version"
