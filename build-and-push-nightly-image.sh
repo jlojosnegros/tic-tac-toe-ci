5df809fb6ac1b7f5dfefb2628967992ffb1b0145
gerrit developer <dev@example.com>
2018-05-16 14:23:01 +0200

    nigthly
    Change-Id: I1647412a40a1db8aaa32017e25bdbee4db546f14

--- build-and-push-nightly-image.sh
+++ build-and-push-nightly-image.sh
@@ -5,8 +5,8 @@ current_pom_version=$(cat ${1})
 date=`date +%Y%m%d`
 string='nightly'
 
-echo "current_pom_version=$current_pom_version"
-echo "date=$date"
+#echo "current_pom_version=$current_pom_version"
+#echo "date=$date"
 
 match_expression='^([0-9]*)\.([0-9]*)\.([0-9]*)-(.*)$'
 
@@ -15,8 +15,8 @@ if [[ ${current_pom_version} =~ $match_expression ]]; then
     tag=${BASH_REMATCH[1]}\.${BASH_REMATCH[2]}\.${BASH_REMATCH[3]}\.${string}\.${date}
 fi
 
-echo "tag=$tag"
+echo $tag
 
-docker build --build-arg GIT_COMMIT=$(git rev-parse HEAD) --build-arg COMMIT_DATE=$(git log -1 --format=%cd --date=format:%Y-%m-%dT%H:%M:%S) -f Dockerfile_Nightly -t practicacimasterupm/curso-ci-practica:${tag} .
+# docker build --build-arg GIT_COMMIT=$(git rev-parse HEAD) --build-arg COMMIT_DATE=$(git log -1 --format=%cd --date=format:%Y-%m-%dT%H:%M:%S) -f Dockerfile_Nightly -t practicacimasterupm/curso-ci-practica:${tag} .
 
-docker push practicacimasterupm/curso-ci-practica:${tag}
+# docker push practicacimasterupm/curso-ci-practica:${tag}
